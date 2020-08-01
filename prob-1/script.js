9cf990c99c0d3792c8214de9212db98a543a53a9
saakeeb <saakeeb752@gmail.com>
2020-08-01 06:15:50 +0600

    return added

--- prob-1/script.js
+++ prob-1/script.js
@@ -3,11 +3,7 @@ const anagrams = (stringA, stringB) =>{
     stringA = stringA.replace(/[^\w]/ig, '').toLowerCase()
     stringB = stringB.replace(/[^\w]/ig, '').toLowerCase()
   
-    return sorts(stringA) === sorts(stringB)
-  }
+    return stringA.split('').sort().join('') === stringB.split('').sort().join('')
+}
   
-  
-  const sorts = string => {
-    return string.split('').sort().join('');
-  }
   console.log(anagrams('Sakib', 'BisaK'));
